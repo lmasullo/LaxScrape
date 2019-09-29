23549e8514a801c0171c664f52cabc4b8e0df88b
Larry Masullo <lmasullo@gmail.com>
2019-09-29 15:21:23 -0500

    Setting up mongo connection

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Dotenv to store passwords
+// require('dotenv').config();
+
 const express = require('express');
 // const logger = require('morgan');
 const mongoose = require('mongoose');
@@ -9,9 +12,9 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 // Require all models
-// var db = require("./models");
+// let db = require('./models');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Initialize Express
 const app = express();
@@ -25,6 +28,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static('public'));
+
+// Connect to the Mongo DB
+// mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+
+const url =
+  'mongodb://heroku_6m9t9glz:Laxman27@ds153824.mlab.com:53824/heroku_6m9t9glz';
+
+// const url2 = 'mongodb://localhost/laxnews';
+
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/laxnews';
+const MONGODB_URI = url || 'mongodb://localhost/laxnews';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Start the server
 app.listen(PORT, function() {
